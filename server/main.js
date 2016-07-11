38226f2d3d44a040682b6047dbe393ce5cdc4933
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 23:27:30 +0700

    update main server

--- server/main.js
+++ server/main.js
@@ -1,5 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import { Chats } from '../imports/api/chats';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (Chats.find().count() === 0) {
+    const chats = [{
+      'id': '1',
+      'nama': 'rizki',
+      'pesan': 'hy...',
+      'waktu': new Date()
+    }, {
+      'id': '2',
+      'nama': 'mufrizal',
+      'pesan': 'hy juga...',
+      'waktu': new Date()
+    }];
+
+    chats.forEach((chat) => {
+      Chats.insert(chat)
+    });
+  }
+
 });
