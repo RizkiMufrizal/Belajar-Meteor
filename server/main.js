c6cdc1172b8909540410ddce0bc190572fc7806a
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 23:39:10 +0700

    update waktu

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Chats } from '../imports/api/chats';
 
 Meteor.startup(() => {
+  Chats.remove({});
   if (Chats.find().count() === 0) {
     const chats = [{
       'id': '1',
@@ -12,7 +13,7 @@ Meteor.startup(() => {
       'id': '2',
       'nama': 'mufrizal',
       'pesan': 'hy juga...',
-      'waktu': new Date()
+      'waktu': new Date("October 13, 2015 11:13:00")
     }];
 
     chats.forEach((chat) => {
