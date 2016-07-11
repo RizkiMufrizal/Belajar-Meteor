77cc010b2c243c7271b8242f572b694a049b85a1
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-12 00:10:00 +0700

    update main server

--- server/main.js
+++ server/main.js
@@ -5,12 +5,10 @@ Meteor.startup(() => {
   Chats.remove({});
   if (Chats.find().count() === 0) {
     const chats = [{
-      'id': '1',
       'nama': 'rizki',
       'pesan': 'hy...',
       'waktu': new Date()
     }, {
-      'id': '2',
       'nama': 'mufrizal',
       'pesan': 'hy juga...',
       'waktu': new Date("October 13, 2015 11:13:00")
